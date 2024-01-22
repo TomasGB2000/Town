0e78719445db8a2b7c8da0b14387888b0e3b0fbf
Tobias Noelkensmeier <Tobiassherco125@gmail.com>
2024-01-22 18:11:37 +0200

    Tobi - Added Movement script

--- FPS Environment/Assets/PlayerMovement.cs
+++ FPS Environment/Assets/PlayerMovement.cs
@@ -1,27 +1,74 @@
-using System.Collections;
-using System.Collections.Generic;
+
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public CharacterController controller;
+    public float moveSpeed = 15f;
+    private Rigidbody rb;
+    public float jumpForce = 10f;
 
-    public float speed = 12f;
+    private float fwdInput;
+    private float horizontalInput;
+    private bool isGrounded = true;
+
+    private float gravity = 9.81f;
+    
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        
+        ///Move();
 
-        Vector3 move = transform.right * x + transform.forward * z;
+        //player movement
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        fwdInput = Input.GetAxisRaw("Vertical");
+        rb.AddRelativeForce(Vector3.forward * moveSpeed * Time.deltaTime * fwdInput, ForceMode.Impulse);
+        rb.AddRelativeForce(Vector3.right * moveSpeed * Time.deltaTime * horizontalInput, ForceMode.Impulse);
+        if (!isGrounded)
+        {
+            moveSpeed = 5f;
+            rb.drag = 0f;
+        }
+        else
+        {
+            rb.drag = 5f;
+            moveSpeed = 50f;
+        }
 
-        controller.Move(move * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftShift) && isGrounded)
+        {
+            moveSpeed = 100f;
+        }
+
+        //player jumping
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, Mathf.Sqrt(2f * jumpForce * gravity), rb.velocity.z);
+            isGrounded = false;
+        }
+
+        // Apply gravity to simulate a more realistic jump
+        rb.velocity += Vector3.up * Physics.gravity.y * Time.deltaTime;
+
+        // Limit the upward velocity to make the jump less floaty
+        if (rb.velocity.y > 0f && rb.velocity.y > jumpForce)
+        {
+            rb.velocity = new Vector3(rb.velocity.x,  jumpForce, rb.velocity.z);
+        }
+        
+    }
+
+    public void OnCollisionEnter(Collision collision){
+        if(collision.gameObject.CompareTag ("Ground")){
+            isGrounded = true;
+        }
     }
 }
